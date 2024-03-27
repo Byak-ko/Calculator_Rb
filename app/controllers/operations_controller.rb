9ac2107d7c8084cf640674c96f29aa501a152a40
Byak-ko <maxim993511@gmail.com>
2024-03-27 12:34:53 +0200

    Initial commit

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,21 +1,18 @@
 class OperationsController < ApplicationController
+
+
   def new
     @operation = Operation.new
   end
-#  def create
-#    @operation = Operation.new(operation_params)
-#
-#    if @operation.valid?
-#      @operation.calculate!
-#      render :new, status: :ok
-#    else
-#      render :new, status: :unprocessable_entity
-#    end
-#  end
-#
-#  private
-#
-#  def operation_params
-#    params.require(:operation).permit(:expression)
-#  end
+
+  def create
+    @operation = Operation.build(params[:operation])
+
+    if @operation.valid?
+      @operation.calculate!
+      render :new, status: :ok
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,44 +1,71 @@
 class Operation
-    include ActiveModel::Model
-  
-    attr_accessor :expression, :result
-  
-    validate :valid_expression?
-  
-    def message
-      @expression.present? ? @expression : ""
-    end
-  
-  
- #   def valid_expression?
- #     return if expression.blank?
- # 
- #     unless /\A\d+(\s*[\+\-\*]\s*\d+)*\z/.match?(expression)
- #       errors.add(:expression, "is not a valid expression")
- #     end
- #   end
- #   
- #   def calculate!
- #       operands, operators = parse_expression
- #       self.result = operands.first.to_i
- #   
- #       operators.each_with_index do |operator, index|
- #         operand = operands[index + 1].to_i
- #         case operator
- #         when "+"
- #           self.result += operand
- #         when "-"
- #         end
- #       end
- #     end
-#
- #   def parse_expression
- #     operands = expression.scan(/\d+/)
- #     operators = expression.scan(/[\+\-\*]/)
- #     [operands, operators]
- #   end
+  include ActiveModel::Model
 
-  
+  attr_accessor(:operand_one, :operand_two, :operation, :result)
 
+  validates :operand_one, presence: true
+  validates :operand_two, presence: true
+  validates :operation, presence: true
+  validates :operation, inclusion: {
+    in: %w(+ - *),
+    message: "%{value} is not a valid operation"
+  }
+
+
+  def self.build(*args)
+    first, op, second = args.flatten.map(&:chomp).reject(&:blank?).take(3)
+    new(operand_one: first&.to_i, operand_two: second&.to_i, operation: op)
   end
+
+=begin
+  def self.build(args)
+    operand_one = ""
+    operation = ""
+    operand_two = ""
   
+    args.each_with_index do |arg, index|
+      if arg.match?(/\d/) && operation.empty?
+        operand_one << arg
+      elsif arg.match?(/[\+\-\*\/]/) && operand_one.empty?
+        operand_one = arg
+      elsif arg.match?(/[\+\-\*\/]/) && !operand_one.empty?
+        operation = arg
+      elsif arg == "="
+        operand_two = args[(index + 1)..-1].join
+      end
+    end
+    new(operand_one: operand_one.to_i, operand_two: operand_two.to_i, operation: operation)
+  end
+=end
+
+  def calculate!
+    __send__(operation.to_s)
+  end
+
+  def message
+    @operand_one ? "#{@operand_one} #{@operation} #{@operand_two}" : ""
+  end
+
+  private
+
+    def +
+      @result = operand_one + operand_two
+      reset
+    end
+
+    def -
+      @result = operand_one - operand_two
+      reset
+    end
+
+    def *
+      @result = operand_one * operand_two
+      reset
+    end
+
+    def reset
+      @operand_one = nil
+      @operand_two = nil
+      @operation = nil
+    end
+end
